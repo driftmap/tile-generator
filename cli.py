5f5f3c35bfe9d53a10aef20909ab54f4bc9dc263
maybemkl <mikael.brunila@gmail.com>
2021-06-10 16:38:47 -0400

    cli for tile generation

---
+++ cli.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from src.tile_generator import TileGenerator
+
+import click
+import os
+
+@click.group()
+def main():
+    pass
+
+@click.command()
+@click.argument('city_key')
+def tilegen(city_key):
+    click.echo(f'Launching tilegenerator for city key "{city_key}."')
+    load_dotenv()
+    OMT = os.environ.get("OMT_URL")
+    city_name = "Atlanta, Fulton County, Georgia, United States"
+    filename = str(os.environ.get("DATA_PATH_CITY")) + "us_tile_tree.json"
+    outpath = "data/tiles/"
+    tg = TileGenerator(OMT, city_key, city_name ,filename,outpath)
+    tg.generate_tiles()
+
+main.add_command(tilegen)
+
+if __name__ == '__main__':
+    main()
