c06da8f526dafcf8a7fc58ba77468f73eba72fa5
maybemkl <mikael.brunila@gmail.com>
2021-06-11 13:45:18 -0400

    add geocoder, update todo

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from src.tile_generator import TileGenerator
+from src.city_geocoder import Geocoder
 
 import click
 import os
@@ -8,9 +9,9 @@ load_dotenv()
 
 """
 TODO:
-- Add arguments for filename & outpath
+- Add arguments for filename & outpath, z range, top_n etc
 - Add command for geocoding
-- Add help 
+- Add help
 
 """
 
@@ -20,7 +21,7 @@ def main():
 
 @click.command()
 @click.argument('city_key')
-def tilegen(city_key):
+def gentiles(city_key):
     click.echo(f'Launching tilegenerator for city key "{city_key}."')
     OMT = os.environ.get("OMT_URL")
     city_name = "Atlanta, Fulton County, Georgia, United States"
@@ -29,7 +30,22 @@ def tilegen(city_key):
     tg = TileGenerator(OMT,city_key,city_name,filename,outpath)
     tg.generate_tiles()
 
-main.add_command(tilegen)
+@click.command()
+@click.argument('region')
+def gentiletree(region):
+    click.echo(f'Launching geocoder top cities."')
+    API_KEY = os.environ.get("GOOGLE_MAPS_KEY")
+    top_n = 10
+    DATA_PATH = 'data/cities/'
+    z_low = 5
+    z_high = 15
+    filename = f'{DATA_PATH}uscities.csv'
+    outpath = f'{DATA_PATH}us_tile_tree.json'
+    gc = Geocoder(API_KEY, region, filename, z_low, z_high, top_n, outpath)
+    gc.geocode()
+
+main.add_command(gentiles)
+main.add_command(gentiletree)
 
 if __name__ == '__main__':
     main()
